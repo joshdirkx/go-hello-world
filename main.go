3c73822db946fbfcf7e6a1afc1f4a244d349379e
Josh Dirkx <joshdirkx@gmail.com>
2023-01-20 17:06:01 -0800

    update spacing

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+  "fmt"
+  "net/http"
 )
 
 func main() {
-	http.HandleFunc(
-		"/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello World")
-		},
-	)
+  http.HandleFunc(
+    "/", func(w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf(w, "Hello World")
+    },
+    )
 
-	_ = http.ListenAndServe(":8080", nil)
+  _ = http.ListenAndServe(":8080", nil)
 }
